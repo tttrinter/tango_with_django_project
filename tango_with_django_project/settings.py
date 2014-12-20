dc30a483737ed56991fbf3c786e4d5473fc632dc
Tom <tttrinter@gmail.com>
2014-12-20 08:18:19 -0500

    New files for Static media.

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -10,8 +10,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from django.conf.global_settings import TEMPLATE_DIRS, STATICFILES_DIRS
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -22,7 +22,17 @@ SECRET_KEY = 'rnaf736an9u=it&3hzr@gg=e(^m6o&ld4=2!_2u6@+o-^vu7+b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
+
+#STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [STATIC_PATH,]
+
 TEMPLATE_DEBUG = True
+
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+
+TEMPLATE_DIRS = [TEMPLATE_PATH,]
 
 ALLOWED_HOSTS = []
 
